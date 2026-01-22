3a12ac40b42ede23a23f00bcb2bf020ff0091b06
Lucaas_lpp <lepape.lucas@outlook.com>
2026-01-22 12:16:51 +0100

    exercice 5 et début 6 mais sans node

--- TD1/script.js
+++ TD1/script.js
@@ -5,12 +5,27 @@ function getRandomIntInclusive(min, max) {
 }
 
 var source = document.getElementById('source');
+var box = document.getElementById('box');
+var info = document.getElementById('info');
 let compteur = 0
 
 
 const repetition = setInterval(() => {
-  source.innerText = getRandomIntInclusive(-10, 40)
+  let value = getRandomIntInclusive(-10, 40)
+  source.innerText = value
   compteur++
+
+  if (-10 <= value && value <= 0) {
+    box.style.border = '2px solid blue'
+    info.innerText = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !"
+  } else if (0 < value && value <= 20)
+    box.style.border = '2px solid green'
+  else if (20 < value && value <= 30) {}
+    box.style.border = '2px solid orange'
+  else if (30 < value && value <= 40) {
+    box.style.border = '2px solid red'
+    info.innerText = "Caliente ! Vamos a la playa, ho hoho hoho !!"
+  }
 
   if (compteur == 20) {
     clearInterval(repetition)
